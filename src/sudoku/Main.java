6241a78768de217cb8d022781b03a8e8d588b70e
GeryRachmadi <gerymcpe@gmail.com>
2024-12-06 16:12:41 +0700

    Commit

---
+++ src/sudoku/Main.java
@@ -0,0 +1,25 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #7
+ * 1 -  5026221163 - Mohammad Geresidi Rachmadi
+ * 2 -  5026221187 - Muhammad Irsyad Fahmi
+
+ */
+package sudoku;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Sudoku");
+        Sudoku game = new Sudoku();
+
+
+    }
+
+}
---
+++ src/sudoku/SudokuConstants.java
@@ -0,0 +1,19 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #7
+ * 1 -  5026221163 - Mohammad Geresidi Rachmadi
+ * 2 -  5026221187 - Muhammad Irsyad Fahmi
+
+ */
+package sudoku;
+/**
+ * Define the named constants used in many classes.
+ */
+public class SudokuConstants {
+    /** Size of the board */
+    public static final int GRID_SIZE = 9;
+    /** Size of sub-grid of the board */
+    public static final int SUBGRID_SIZE = 3;
+}
